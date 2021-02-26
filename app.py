47737556d4d55a4a12d92444c9aa7d04037a481d
Jens Hegner Stærmose <jensstaermose@hotmail.com>
2021-02-26 11:56:10 +0100

    Getting online players in status

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ import os
 import math
 import psutil
 import discord
+import requests
 from dotenv import load_dotenv
 from discord.ext.commands import Bot
 
 load_dotenv()
 DISCORD_APP_TOKEN = os.getenv('DISCORD_APP_TOKEN')
+SERVER_URL = os.getenv('SERVER_URL')
 
 client = Bot(description="Hutli's Server Status",
              command_prefix="!", pm_help=False, )
@@ -21,6 +23,8 @@ async def on_ready():
 async def srvstatus(ctx):
     if ctx.message.author.guild_permissions.administrator:
         msg = ''
+        info = requests.get(SERVER_URL).json()
+        msg += f'Players: {info["OnlinePlayers"]}/{info["TotalPlayers"]}\n\n'
         msg += f'Memory useage: {psutil.virtual_memory().percent}%\n\nCPU Cores:\n'
         for percentage in psutil.cpu_percent(percpu=True, interval=1):
             percentage_denominator = 10
